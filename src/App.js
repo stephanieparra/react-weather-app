9165783321c1b8f4219c3062dec9375d620cf7c2
stephanieparra <stephanieparrastudio@gmail.com>
2022-05-30 15:25:06 -0700

    Added portfolio link

--- src/App.js
+++ src/App.js
@@ -7,7 +7,14 @@ export default function App() {
       <h1>Weather App</h1>
       <SearchEngine />
       <p>
-        This project was coded by Stephanie Parra and is{" "}
+        This project was coded by{" "}
+        <a
+          href="https://optimistic-torvalds-560c7b.netlify.app"
+          target="_blank"
+        >
+          Stephanie Parra
+        </a>{" "}
+        and is{" "}
         <a
           href="https://github.com/stephanieparra/react-weather-app"
           target="_blank"
