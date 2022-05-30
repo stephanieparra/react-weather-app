e6423932b978a7b5cd2dd6f290b7e700ddeae4d6
stephanieparra <stephanieparrastudio@gmail.com>
2022-05-30 15:22:30 -0700

    Deleted items and added a footer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,15 @@ export default function App() {
     <div className="App">
       <h1>Weather App</h1>
       <SearchEngine />
+      <p>
+        This project was coded by Stephanie Parra and is{" "}
+        <a
+          href="https://github.com/stephanieparra/react-weather-app"
+          target="_blank"
+        >
+          open-sourced.
+        </a>
+      </p>
     </div>
   );
 }
