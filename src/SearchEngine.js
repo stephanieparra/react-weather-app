8f948565914923a1fa2725bc7026a505c46e4659
stephanieparra <stephanieparrastudio@gmail.com>
2022-09-26 17:48:42 -0700

    Changed css for image and temperature

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -54,18 +54,20 @@ export default function SearchEngine() {
     return (
       <div>
         {form}
-        <div className="row">
+        <div className="row mt-3">
           <div className="col-6">
             <h2>Los Angeles</h2>
             <ul>
               <li>Wednesday 7:00</li>
-              <li>Description: {weather.description}</li>
+              <li>{weather.description}</li>
             </ul>
-            <img src={weather.icon} alt={weather.description} />
-            <span className="temperature">
-              {Math.round(weather.temperature)}
-            </span>
-            <span className="unit">°C</span>
+            <div className="clear-fix">
+              <img src={weather.icon} alt={weather.description} className="float-left" />
+              <span className="temperature">
+                {Math.round(weather.temperature)}
+              </span>
+              <span className="unit">°C</span>
+            </div>
           </div>
           <div className="col-6">
             <ul>
