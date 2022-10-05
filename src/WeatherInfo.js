9d4cc7244f39d2d564e3f54256ce8aa743db578d
stephanieparra <stephanieparrastudio@gmail.com>
2022-10-04 21:37:47 -0700

    Update col under row in WeatherInfo

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -15,22 +15,22 @@ export default function WeatherInfo(props) {
       </ul>
       <div className="row mt-3">
         <div className="col-6">
-          <div className="clear-fix">
-            <div className="float-left">
-              <WeatherIcon code={props.data.icon} />
+          <div className="d-flex">
+            <div>
+              <WeatherIcon code={props.data.icon} size={52} />
             </div>
 
-            <div className="float-left">
+            <div>
               <WeatherTemperature celsius={props.data.temperature} />
             </div>
           </div>
+          <div className="col-6">
+            <ul>
+              <li>Humidity: {props.data.humidity}%</li>
+              <li>Wind: {props.data.wind}/km</li>
+            </ul>
+          </div>
         </div>
-      </div>
-      <div className="col-6">
-        <ul>
-          <li>Humidity: {props.data.humidity}%</li>
-          <li>Wind: {props.data.wind}/km</li>
-        </ul>
       </div>
     </div>
   );
