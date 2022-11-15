2341a5aab91b4f84b863880e60fef9987484ed9a
stephanieparra <stephanieparrastudio@gmail.com>
2022-11-14 18:24:51 -0800

    Fixed typo

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -25,7 +25,7 @@ export default function WeatherForecastDay(props) {
     <div>
       <div className="WeatherForecast-day">{day()}</div>
       <WeatherIcon code={props.data.condition.icon} size={36} />
-      <div classname="WeatherForecast-temperatures">
+      <div className="WeatherForecast-temperatures">
         <span className="WeatherForecast-temperature-max">
           {maxTemperature()}
         </span>
